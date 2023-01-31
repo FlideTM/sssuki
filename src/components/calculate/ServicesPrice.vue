835f5a8ff1ede178ad0247ee0104716f719e5c98
Felix <zykinf@gmail.com>
2023-01-31 19:02:29 +0500

    initial commit

--- src/components/calculate/ServicesPrice.vue
+++ src/components/calculate/ServicesPrice.vue
@@ -2,19 +2,75 @@
     <div class="Popup">
         <div class="Block">
             <div id="close"><span @click="this.$emit('closeSP')"><Icon icon="ri:close-circle-fill" /></span></div>
-
             
-
+            <div class="newsletter">
+                <select v-model="selected">
+                    <option>Telegram</option>
+                    <option>Instagram</option>
+                    <option>Vk</option>
+                    <option>WhatsApp</option>
+                </select>
+                <input v-model="count" type="number" placeholder="Укажите количество сообщений/услуг" />
+                <input type="button" @click="getPrice(selected, count)" value="button" />
+                {{ result }}
+            </div>
+            <div class="another">
+                <select v-model="selected1">
+                    <option>Боты</option>
+                    <option>Дизайн социальных сетей</option>
+                </select>
+                <select v-model="socnet" v-if="selected1 == 'Боты'">
+                    <option>Telegram</option>
+                    <option>Instagram</option>
+                    <option>Vk</option>
+                    <option>WhatsApp</option>
+                </select>
+                <input type="button" @click="getPrice(selected1, socnet)" value="button" />
+            </div>
         </div>
         <div class="Background" @click="this.$emit('closeSP')"></div>
     </div>
 </template>
 <script>
 import { Icon } from '@iconify/vue';
+import prices from '@/assets/price.json';
 export default{
     components:{
         Icon,
     },
+    data(){
+        return{
+            servivce: prices.services,
+            selected: '',
+            selected1: '',
+            count: '',
+            result: '',
+            socnet: '',
+        }
+    },
+    methods:{
+        getPrice(SocNet, Count){
+            let sum = this.servivce.newsletter;
+            switch(SocNet){
+                case "Telegram":
+                    this.result = sum.telegram * Count;
+                    break;
+                case "Instagram":
+                    this.result = sum.instagram * Count;
+                    break;
+                case "Vk":
+                    this.result = sum.vk * Count;
+                    break;
+                case "WhatsApp":
+                    this.result = sum.whatsapp * Count;
+                    break;
+                case "Боты":
+                    break;
+                case "Дизайн социальных сетей":
+                    break;
+            }
+        }
+    }
 }
 </script>
 <style scoped>
